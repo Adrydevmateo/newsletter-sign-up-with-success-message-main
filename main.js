d3f7ddf0accb2296e2c29e247d4924024e2abf23
AdryDev <adrydevmateo@gmail.com>
2024-05-25 11:52:54 -0400

    Finished mobile design

--- main.js
+++ main.js
@@ -2,11 +2,26 @@
 
 const form = document.getElementById('form-subscribe')
 
+const validateEmail = (email) => {
+ const emailRegex = /^([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/;
+ return emailRegex.test(email);
+}
+
+form.addEventListener('input', () => {
+ if (form.classList.contains('invalid')) form.classList.remove('invalid')
+})
+
 form.addEventListener('submit', (e) => {
  e.preventDefault()
 
  const formData = new FormData(form)
  const email = formData.get('email')
+ const isValid = validateEmail(email)
+
+ if (!email || !isValid) {
+  form.classList.add('invalid')
+  return new Error('Invalid Email')
+ }
 
  const reqBody = {
   name: 'Email',
